afe643ed65fcbd717f032c5096eb07c50f6cb011
Niki Skene <ns@iacy.com>
2026-07-25 18:36:54 +0200

    Updated InstituteShell.tsx

--- src/components/institute/InstituteShell.tsx
+++ src/components/institute/InstituteShell.tsx
@@ -1,6 +1,7 @@
 // src/components/institute/InstituteShell.tsx
 
 import { PropsWithChildren } from 'react';
+import InstituteBackground from './InstituteBackground';
 import InstituteHeader from './InstituteHeader';
 import { useImpactScale } from '../../providers/ImpactScaleProvider';
 import { getInstituteTheme } from '../../theme/instituteTheme';
@@ -18,18 +19,23 @@ export default function InstituteShell({
         background: theme.background,
         color: theme.text,
         transition:
-          'background-color .9s ease,color .9s ease',
+          'background-color .9s ease, color .9s ease',
+        position: 'relative',
+        overflow: 'hidden',
       }}
     >
+      <InstituteBackground />
+
       <div
         style={{
+          position: 'relative',
+          zIndex: 1,
           maxWidth: theme.maxWidth,
           margin: '0 auto',
           padding: '0 40px 120px',
         }}
       >
         <InstituteHeader />
-
         {children}
       </div>
     </div>
