ec4622384c72c4578469c39b452f8a10fe7fe8ab
Niki Skene <ns@iacy.com>
2026-07-25 18:35:34 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+// src/App.tsx
+
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './hooks/useAuth';
+import { ImpactScaleProvider } from './providers/ImpactScaleProvider';
 import AppRoutes from './routes/AppRoutes';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
+      <ImpactScaleProvider>
         <AppRoutes />
-      </AuthProvider>
+      </ImpactScaleProvider>
     </BrowserRouter>
   );
 }
