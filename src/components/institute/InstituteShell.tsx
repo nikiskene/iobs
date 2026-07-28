729f59cf4295ac8ec6f55a9890e44d538b0ecd11
Niki Skene <ns@iacy.com>
2026-07-28 15:09:33 +0200

    Updated InstituteShell.tsx

--- src/components/institute/InstituteShell.tsx
+++ src/components/institute/InstituteShell.tsx
@@ -1,10 +1,13 @@
 // src/components/institute/InstituteShell.tsx
 
 import { PropsWithChildren } from 'react';
-import InstituteBackground from './InstituteBackground';
-import InstituteHeader from './InstituteHeader';
 import { useImpactScale } from '../../providers/ImpactScaleProvider';
 import { getInstituteTheme } from '../../theme/instituteTheme';
+
+import InstituteBackground from './InstituteBackground';
+import InstituteEffects from './InstituteEffects';
+import InstituteHeader from './InstituteHeader';
+import PageFadeIn from './PageFadeIn';
 
 export default function InstituteShell({
   children,
@@ -16,27 +19,30 @@ export default function InstituteShell({
     <div
       style={{
         minHeight: '100vh',
+        position: 'relative',
+        overflow: 'hidden',
         background: theme.background,
         color: theme.text,
         transition:
-          'background-color .9s ease, color .9s ease',
-        position: 'relative',
-        overflow: 'hidden',
+          'background-color .8s ease, color .8s ease',
       }}
     >
       <InstituteBackground />
 
+      <InstituteEffects />
+
       <div
         style={{
           position: 'relative',
-          zIndex: 1,
+          zIndex: 10,
           maxWidth: theme.maxWidth,
           margin: '0 auto',
           padding: '0 40px 120px',
         }}
       >
         <InstituteHeader />
-        {children}
+
+        <PageFadeIn>{children}</PageFadeIn>
       </div>
     </div>
   );
