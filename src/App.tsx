e7fd8d7cce736fc6810e3664ac99f2c2ca675a8c
Niki Skene <ns@iacy.com>
2026-08-14 13:08:15 +0200

    Mount route scroll controller

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import { ImpactScaleProvider } from './providers/ImpactScaleProvider';
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './routes/ScrollToTop';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <ImpactScaleProvider>
           <AppRoutes />
