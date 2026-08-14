9c678e309a031e1ae3ceccdacd408100cc4d62b9
Niki Skene <ns@iacy.com>
2026-08-14 13:06:32 +0200

    Restore partner admin route

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,15 +1,11 @@
 // src/routes/AdminRoutes.tsx
-// src/routes/AdminRoutes.tsx
 import { Route, Navigate } from 'react-router-dom';
 
 import Navigation from '../components/Navigation';
 import DashboardLayout from '../components/DashboardLayout';
-
 import AdminRoute from './AdminRoute';
-
 import AdminHome from '../pages/admin/AdminHome';
 import AdminCategoryPage from '../pages/admin/AdminCategoryPage';
-
 import HomepageCMS from '../pages/admin/HomepageCMS';
 import AboutCMS from '../pages/admin/AboutCMS';
 import CategoriesAdmin from '../pages/admin/CategoriesAdmin';
@@ -18,7 +14,7 @@ import TeamAdmin from '../pages/admin/TeamAdmin';
 import SiteSettingsAdmin from '../pages/admin/SiteSettingsAdmin';
 import GlossaryAdmin from '../pages/admin/GlossaryAdmin';
 import ScaleWorldsAdmin from '../pages/admin/ScaleWorldsAdmin';
-
+import PartnerLogosAdmin from '../pages/admin/PartnerLogosAdmin';
 import EventsAdmin from '../pages/admin/events/EventsAdmin';
 import ExpeditionsAdmin from '../pages/admin/expeditions/ExpeditionsAdmin';
 import ExpeditionInquiriesAdmin from '../pages/admin/expeditions/ExpeditionInquiriesAdmin';
@@ -34,61 +30,34 @@ import IdentitySettingsPage from '../pages/admin/identity/IdentitySettingsPage';
 
 export default function AdminRoutes() {
   return (
-    <Route
-      path="/admin"
-      element={
-        <AdminRoute>
-          <Navigation />
-          <DashboardLayout />
-        </AdminRoute>
-      }
-    >
+    <Route path="/admin" element={<AdminRoute><Navigation /><DashboardLayout /></AdminRoute>}>
       <Route index element={<AdminHome />} />
-
       <Route path="content" element={<AdminCategoryPage title="Content" />} />
       <Route path="members" element={<AdminCategoryPage title="Members" />} />
-      <Route
-        path="experiences"
-        element={<AdminCategoryPage title="Experiences" />}
-      />
-      <Route
-        path="messaging"
-        element={<AdminCategoryPage title="Messaging" />}
-      />
+      <Route path="experiences" element={<AdminCategoryPage title="Experiences" />} />
+      <Route path="messaging" element={<AdminCategoryPage title="Messaging" />} />
       <Route path="settings" element={<AdminCategoryPage title="Settings" />} />
-
       <Route path="content/homepage" element={<HomepageCMS />} />
       <Route path="content/scale-worlds" element={<ScaleWorldsAdmin />} />
       <Route path="content/about" element={<AboutCMS />} />
       <Route path="content/glossary" element={<GlossaryAdmin />} />
       <Route path="content/theses" element={<ThesesAdmin />} />
       <Route path="content/categories" element={<CategoriesAdmin />} />
-
+      <Route path="content/partners" element={<PartnerLogosAdmin />} />
       <Route path="members/team" element={<TeamAdmin />} />
-
       <Route path="experiences/events" element={<EventsAdmin />} />
       <Route path="experiences/expeditions" element={<ExpeditionsAdmin />} />
-      <Route
-        path="experiences/applications"
-        element={<ExpeditionInquiriesAdmin />}
-      />
-      <Route
-        path="experiences/custom"
-        element={<CustomExpeditionInquiriesAdmin />}
-      />
-
+      <Route path="experiences/applications" element={<ExpeditionInquiriesAdmin />} />
+      <Route path="experiences/custom" element={<CustomExpeditionInquiriesAdmin />} />
       <Route path="messaging/conversations" element={<ConversationsAdmin />} />
       <Route path="messaging/contact" element={<ContactMessagesAdmin />} />
-
       <Route path="identity" element={<IdentityOverview />} />
       <Route path="identity/review" element={<IdentityReview />} />
       <Route path="identity/daily-scan" element={<DailyIdentityScan />} />
       <Route path="identity/settings" element={<IdentitySettingsPage />} />
       <Route path="identity/sources" element={<IdentitySources />} />
       <Route path="identity/method" element={<MethodPage />} />
-
       <Route path="settings/design" element={<SiteSettingsAdmin />} />
-
       <Route path="*" element={<Navigate to="/admin" replace />} />
     </Route>
   );
