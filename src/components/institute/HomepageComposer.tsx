5feca28edf1bb820578a713f4dbf61ab5d4b2610
Niki Skene <ns@iacy.com>
2026-08-14 13:06:11 +0200

    Restore homepage cases

--- src/components/institute/HomepageComposer.tsx
+++ src/components/institute/HomepageComposer.tsx
@@ -2,13 +2,16 @@
 import EntranceHero from './EntranceHero';
 import ScaleStoryPanel from './ScaleStoryPanel';
 import AwardHomepageSections from '../awards/AwardHomepageSections';
+import BeautifulSuccessCases from '../awards/BeautifulSuccessCases';
 import InstituteFooter from './InstituteFooter';
+import './recoveredSections.css';
 
 export default function HomepageComposer() {
   return (
     <>
       <EntranceHero />
       <ScaleStoryPanel />
+      <BeautifulSuccessCases />
       <AwardHomepageSections />
       <InstituteFooter />
     </>
