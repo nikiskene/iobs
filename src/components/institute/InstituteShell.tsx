406d388069c4a3fd5d41e3a497afca0600ed97ae
Niki Skene <ns@iacy.com>
2026-08-14 14:49:36 +0200

    Wrap public site in locale provider

--- src/components/institute/InstituteShell.tsx
+++ src/components/institute/InstituteShell.tsx
@@ -5,6 +5,7 @@ import InstituteBackground from './InstituteBackground';
 import InstituteHeader from './InstituteHeader';
 import { useImpactScale } from '../../providers/ImpactScaleProvider';
 import { AwardSiteContentProvider } from '../../providers/AwardSiteContentProvider';
+import { LocaleProvider } from '../../providers/LocaleProvider';
 import './instituteHome.css';
 import './awardSite.css';
 import './foundingStripFix.css';
@@ -14,14 +15,16 @@ export default function InstituteShell({ children }: PropsWithChildren) {
   const { scale } = useImpactScale();
 
   return (
-    <AwardSiteContentProvider>
-      <div className={`institute-shell scale-${scale}`}>
-        <InstituteBackground />
-        <div className="institute-frame">
-          <InstituteHeader />
-          {children}
+    <LocaleProvider>
+      <AwardSiteContentProvider>
+        <div className={`institute-shell scale-${scale}`}>
+          <InstituteBackground />
+          <div className="institute-frame">
+            <InstituteHeader />
+            {children}
+          </div>
         </div>
-      </div>
-    </AwardSiteContentProvider>
+      </AwardSiteContentProvider>
+    </LocaleProvider>
   );
 }
