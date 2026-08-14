eacc2220e1e2cbca2b32d17712e630a4f5bf6840
Niki Skene <ns@iacy.com>
2026-08-14 14:54:11 +0200

    Localize public footer

--- src/components/institute/InstituteFooter.tsx
+++ src/components/institute/InstituteFooter.tsx
@@ -1,23 +1,23 @@
 // src/components/institute/InstituteFooter.tsx
 import { Link } from 'react-router-dom';
 import { AWARD_ASSETS } from '../../content/awardAssets';
+import { useLocale } from '../../providers/LocaleProvider';
 import PartnerLogoStripe from './PartnerLogoStripe';
 
-const COLUMNS = [
-  ['The Award', [['Categories', '/categories'], ['Judging', '/judging'], ['Nominate', '/nominate'], ['Enter', '/enter']]],
-  ['The Institute', [['Founding Voices', '/voices'], ['Assembly', '/assembly'], ['Experiences', '/expeditions'], ['Contact', '/contact']]],
-  ['Institutional', [['Founding Partners', '/partners'], ['Governance', '/judging'], ['Partner Briefing', '/partners'], ['Contact', '/contact'], ['Login', '/login']]],
-];
-
 export default function InstituteFooter() {
-  return (
-    <>
-      <PartnerLogoStripe />
-      <footer className="award-footer">
-        <div className="award-footer-intro"><img src={AWARD_ASSETS.twoLineDark} alt="Beautiful Success Award" /><p>Celebrating Beautiful Success globally.</p></div>
-        <div className="award-footer-columns">{COLUMNS.map(([title, links]) => <div key={title as string}><h3>{title as string}</h3>{(links as string[][]).map(([label, href]) => <Link to={href} key={label}>{label}</Link>)}</div>)}</div>
-        <div className="award-footer-base"><span>© {new Date().getFullYear()} Institute of Beautiful Success</span><span>Founding Edition</span></div>
-      </footer>
-    </>
-  );
+  const { t } = useLocale();
+  const columns = [
+    [t('footer.award'), [[t('footer.categories'), '/categories'], [t('footer.judging'), '/judging'], [t('footer.nominate'), '/nominate'], [t('footer.enter'), '/enter']]],
+    [t('footer.institute'), [[t('footer.voices'), '/voices'], [t('footer.assembly'), '/assembly'], [t('footer.experiences'), '/expeditions'], [t('footer.contact'), '/contact']]],
+    [t('footer.institutional'), [[t('footer.foundingPartners'), '/partners'], [t('footer.governance'), '/judging'], [t('footer.partnerBriefing'), '/partners'], [t('footer.contact'), '/contact'], [t('footer.login'), '/login']]],
+  ];
+
+  return <>
+    <PartnerLogoStripe />
+    <footer className="award-footer">
+      <div className="award-footer-intro"><img src={AWARD_ASSETS.twoLineDark} alt="Beautiful Success Award" /><p>{t('footer.claim')}</p></div>
+      <div className="award-footer-columns">{columns.map(([title, links]) => <div key={title as string}><h3>{title as string}</h3>{(links as string[][]).map(([label, href]) => <Link to={href} key={`${label}-${href}`}>{label}</Link>)}</div>)}</div>
+      <div className="award-footer-base"><span>© {new Date().getFullYear()} Institute of Beautiful Success</span><span>{t('footer.edition')}</span></div>
+    </footer>
+  </>;
 }
