226ea6af6fa25c88978e7e7b7ee6119ecee51841
Niki Skene <ns@iacy.com>
2026-08-14 14:50:06 +0200

    Add multilingual header and language switcher

--- src/components/institute/InstituteHeader.tsx
+++ src/components/institute/InstituteHeader.tsx
@@ -3,9 +3,11 @@
 import { Link } from 'react-router-dom';
 import { AWARD_ASSETS } from '../../content/awardAssets';
 import { useAwardSiteContent } from '../../providers/AwardSiteContentProvider';
+import { LANGUAGE_OPTIONS, useLocale, type Locale } from '../../providers/LocaleProvider';
 
 export default function InstituteHeader() {
   const { get } = useAwardSiteContent();
+  const { locale, setLocale, t } = useLocale();
   const claim = get('site_claim');
 
   return (
@@ -15,13 +17,18 @@ export default function InstituteHeader() {
           <img src={AWARD_ASSETS.circleDark} alt="Beautiful Success Award" />
         </Link>
         <nav aria-label="Main navigation">
-          <Link to="/award">The Award</Link>
-          <Link to="/categories">Categories</Link>
-          <Link to="/judging">How We Judge</Link>
-          <Link to="/voices">Founding Voices</Link>
-          <Link to="/partners">Partners</Link>
+          <Link to="/award">{t('nav.award')}</Link>
+          <Link to="/categories">{t('nav.categories')}</Link>
+          <Link to="/judging">{t('nav.judging')}</Link>
+          <Link to="/voices">{t('nav.voices')}</Link>
+          <Link to="/partners">{t('nav.partners')}</Link>
         </nav>
-        <Link className="enter-link" to="/nominate">Nominate</Link>
+        <div className="institute-header-actions">
+          <select className="language-switcher" value={locale} onChange={(event) => setLocale(event.target.value as Locale)} aria-label="Language">
+            {LANGUAGE_OPTIONS.map((option) => <option value={option.code} key={option.code}>{option.short}</option>)}
+          </select>
+          <Link className="enter-link" to="/nominate">{t('nav.nominate')}</Link>
+        </div>
       </header>
       {claim?.headline && <p className="institute-claim">{claim.headline}</p>}
     </>
