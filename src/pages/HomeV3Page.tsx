3c96fc091c6abc15c016920eb603fa9c9faf0397
nikiskene <ns@iacy.com>
2026-09-04 14:04:15 +0200

    Remove homepage film overlay

--- src/pages/HomeV3Page.tsx
+++ src/pages/HomeV3Page.tsx
@@ -1,8 +1,7 @@
 import HomeV3 from '../components/home-v3/HomeV3';
-import BeautifulSuccessIntro from '../components/institute/BeautifulSuccessIntro';
 import InstituteShell from '../components/institute/InstituteShell';
 import { ScaleWorldsProvider } from '../providers/ScaleWorldsProvider';
 
 export default function HomeV3Page() {
-  return <ScaleWorldsProvider><InstituteShell><HomeV3 /></InstituteShell><BeautifulSuccessIntro /></ScaleWorldsProvider>;
+  return <ScaleWorldsProvider><InstituteShell><HomeV3 /></InstituteShell></ScaleWorldsProvider>;
 }
